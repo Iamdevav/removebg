8fc50777c5ca52a31210468cdfc45e7aed6e6c56
avipatel <aviavipatel04@gmail.com>
2024-02-23 12:40:08 +0530

    add dependecy

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     
-    # Import and register blueprints here (if you have any)
     from . import routes
     app.register_blueprint(routes.bp)
  
