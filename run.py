56056a6bc55ebf94bbb3481af448dbd890a03965
avipatel <aviavipatel04@gmail.com>
2024-02-23 12:48:24 +0530

    add signal

--- run.py
+++ run.py
@@ -1,5 +1,18 @@
-from app import create_app
+# from app import create_app
  
+# if __name__ == '__main__':
+#     app = create_app()
+#     app.run(debug=True)
+import signal
+import sys
+from app import create_app
+
+def signal_handler(sig, frame):
+    print('Received SIGTERM signal. Exiting gracefully.')
+    sys.exit(0)
+
 if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, signal_handler)
+    
     app = create_app()
     app.run(debug=True)
